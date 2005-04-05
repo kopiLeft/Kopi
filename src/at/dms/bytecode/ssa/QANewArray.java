58e3909ed847c95d798c846b26e4be8dbdf21ce6
taoufik <taoufik@bf1b744e-a3f0-0310-8f4b-f8bdd3f196b6>
2005-04-05 16:49:10 +0000

    Modify the Copyright and Fix the svn properties for .g files

--- src/at/dms/bytecode/ssa/QANewArray.java
+++ src/at/dms/bytecode/ssa/QANewArray.java
@@ -1,10 +1,10 @@
 /*
- * Copyright (C) 1990-2001 DMS Decision Management Systems Ges.m.b.H.
+ * Copyright (C) 1990-2005 DMS Decision Management Systems Ges.m.b.H.
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * $Id: QANewArray.java,v 1.1 2004/07/28 18:43:28 imad Exp $
+ * $Id$
  */
 
 package at.dms.bytecode.ssa;
--- src/at/dms/xkopi/lib/base/Connection.java
+++ src/at/dms/xkopi/lib/base/Connection.java
@@ -1,11 +1,11 @@
 
 /*
- * Copyright (C) 1990-2001 DMS Decision Management Systems Ges.m.b.H.
+ * Copyright (C) 1990-2005 DMS Decision Management Systems Ges.m.b.H.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * License version 2 as published by the Free Software Foundation.
+ *
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,7 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * $Id: Connection.java,v 1.2 2004/09/28 14:45:56 graf Exp $
+ * $Id$
  */
 
 package at.dms.xkopi.lib.base;
