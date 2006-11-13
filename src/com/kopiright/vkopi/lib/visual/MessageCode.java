362447281c9afccdc4bcf65a0508279e7af55921
taoufik <taoufik@bf1b744e-a3f0-0310-8f4b-f8bdd3f196b6>
2006-11-13 11:54:58 +0000

    Upd: changed Register to Registry Add: params handling in MessageCode

--- src/com/kopiright/vkopi/lib/visual/MessageCode.java
+++ src/com/kopiright/vkopi/lib/visual/MessageCode.java
@@ -37,41 +37,80 @@ public class MessageCode {
   // STATIC METHODS
   // ----------------------------------------------------------------------
 
+
+  /**
+   * Returns a message (convenience routine).
+   *
+   * @param     key             the message key
+   * @return    the requested message
+   */
+  public static String getMessage(String key) {
+    return getMessage(key, null);
+  }
+
+  /**
+   * Returns a message (convenience routine).
+   *
+   * @param     key             the message key
+   * @param     param           a message parameter
+   * @return    the requested message
+   */
+  public static String getMessage(String key, Object param) {
+    return getMessage(key, new Object[] {param});
+  }
+
+  /**
+   * Returns a message (convenience routine).
+   *
+   * @param     key             the message ke
+   * @param     param1          the first message parameter
+   * @param     param1          the second message parameter
+   * @return    the requested message
+   */
+  public static String getMessage(String key, Object param1, Object param2) {
+    return getMessage(key, new Object[] {param1, param2});
+  }
+
   /**
    * Returns a message (convenience routine).
    * key must be of the form CCC-DDDDD (exp: KOP-00001)
    * where CCC is a 3 capital character module id
    * and DDDDD a 5 digit message id
    *
-   * @param     key            the message key
+   * @param     key             the message key
+   * @param     params          the array of message parameters
    * @return    the requested message
    */
-  public static String getMessage(String key) {
+  public static String getMessage(String key, Object[] params) {
     LocalizationManager         manager;
-    String                      module;
+    String                      domain;
     String                      ident;
     String                      src;
-
-    if (keyPattern.matcher(key).matches()){
-      module = key.substring(0, 3);
-      ident = key.substring(4, 9);
-      if (Application.getRegister() != null) {
-        src = Application.getRegister().getMessageSource(module);
-      } else {
-        throw new InconsistencyException("No Register set for this application.");
-      }
-    } else {
+    
+    if (!keyPattern.matcher(key).matches()) {
       throw new InconsistencyException("Malformed message key '" + key + "'");
     }
-    if (src == null) {
-      throw new InconsistencyException("No message source found for module '" + module + "'");
+    domain = key.substring(0, 3);
+    ident = key.substring(4, 9);
+    if (Application.getRegistery() == null) {
+      throw new InconsistencyException("No Registery set for this application.");
     }
-
+    src = Application.getRegistery().getMessageSource(domain);
+    if (src == null) {
+      throw new InconsistencyException("No message source found for module '"
+                                       + domain + "'");
+    }
+    
     try {
+      String            format;
+      
       manager = new LocalizationManager(Locale.getDefault());
-      return manager.getMessageLocalizer(src, ident).getText();
+
+      // Within a String, "''" represents a single quote in java.text.MessageFormat.
+      format = manager.getMessageLocalizer(src, ident).getText().replaceAll("'", "''");
+      return key + ": " + MessageFormat.format(format, params);
     } catch (InconsistencyException e) {
-      return "!" + key + "!";
+      return key + ": " + "message for !" + key + "! not found!";
     }
   }
   
