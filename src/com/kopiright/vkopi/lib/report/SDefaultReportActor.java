c6493ba1127ae5c1d7fc4281299bb75833d0dacd
wael <wael@bf1b744e-a3f0-0310-8f4b-f8bdd3f196b6>
2007-04-21 11:02:25 +0000

    svn ps.

--- src/com/kopiright/vkopi/lib/report/SDefaultReportActor.java
+++ src/com/kopiright/vkopi/lib/report/SDefaultReportActor.java
@@ -14,7 +14,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * $Id: SActor.java 27892 2007-02-16 16:09:48Z graf $
+ * $Id$
  */
 
 package com.kopiright.vkopi.lib.report;
