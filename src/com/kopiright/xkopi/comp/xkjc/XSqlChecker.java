9a0086a04f149c0b30fc1b21d15872b88799ef84
wael <wael@bf1b744e-a3f0-0310-8f4b-f8bdd3f196b6>
2007-08-09 06:53:26 +0000

    FIX : in typed select statements , "AS " is not written in the sql of the generated class.

--- src/com/kopiright/xkopi/comp/xkjc/XSqlChecker.java
+++ src/com/kopiright/xkopi/comp/xkjc/XSqlChecker.java
@@ -129,8 +129,11 @@ public class XSqlChecker extends com.kopiright.xkopi.comp.sqlc.SqlChecker implem
         }
       }
     }
-
     sqlExpr.accept(this);
+    if (hasAs) {
+      current.append(" AS ");
+      current.append(ident);
+    }
   }
 
   /**
