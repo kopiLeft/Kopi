8b26ad6bc1529935432f79e16aab01ae4be17a5e
taoufik <taoufik@bf1b744e-a3f0-0310-8f4b-f8bdd3f196b6>
2007-09-10 09:46:14 +0000

    Fix: counting average using leaf nodes instead of direct children.

--- src/com/kopiright/vkopi/lib/report/Triggers.java
+++ src/com/kopiright/vkopi/lib/report/Triggers.java
@@ -19,6 +19,8 @@
 
 package com.kopiright.vkopi.lib.report;
 
+import com.kopiright.xkopi.lib.type.NotNullFixed;
+
 /**
  * This class implements predefined report triggers
  */
@@ -30,20 +32,21 @@ public class Triggers {
    */
   public static VCalculateColumn sumInteger(VReportColumn c) {
     return new VCCDepthFirstCircuitN() {
-	public Object evalNode(VReportRow row, int column) {
-	  int		childCount = row.getChildCount();
-	  int		result = 0;
+      public Object evalNode(VReportRow row, int column) {
+        int     childCount = row.getChildCount();
+        int     result = 0;
 
-	  for (int i = 0; i < childCount; i++) {
-	    VReportRow	child = (VReportRow)row.getChildAt(i);
-	    Integer	value = (Integer)child.getValueAt(column);
-	    if  (value != null) {
-	      result += value.intValue();
-	    }
-	  }
-	  return new Integer(result);
-	}
-      };
+        for (int i = 0; i < childCount; i++) {
+          VReportRow    child = (VReportRow)row.getChildAt(i);
+          Integer       value = (Integer)child.getValueAt(column);
+
+          if  (value != null) {
+            result += value.intValue();
+          }
+        }
+        return new Integer(result);
+      }
+    };
   }
 
   /**
@@ -51,31 +54,30 @@ public class Triggers {
    */
   public static VCalculateColumn countInteger(VReportColumn c) {
     return new VCCDepthFirstCircuitN() {
-	public Object evalNode(VReportRow row, int column) {
-	  int		childCount = row.getChildCount();
+      public Object evalNode(VReportRow row, int column) {
+        int     childCount = row.getChildCount();
 
-          if (row.getLevel() > 1) {
-            // the value of a node is the sum of
-            // other nodes (contain no leafs)
-            int         result = 0;
+        if (row.getLevel() > 1) {
+          // the value of a node is the sum of
+          // other nodes (contain no leafs)
+          int   result = 0;
 
-            for (int i = 0; i < childCount; i++) {
-              VReportRow        child = (VReportRow)row.getChildAt(i);
-              Integer           value = (Integer)child.getValueAt(column);
+          for (int i = 0; i < childCount; i++) {
+            VReportRow  child = (VReportRow)row.getChildAt(i);
+            Integer     value = (Integer)child.getValueAt(column);
 
-              if  (value != null) {
-                result += value.intValue();
-              }
+            if  (value != null) {
+              result += value.intValue();
             }
-            return new Integer(result);
-          } else {
-            // the value is the number of the childs
-            // if the childs are leafs
-            return new Integer(childCount);
           }
-	}
-
-      };
+          return new Integer(result);
+        } else {
+          // the value is the number of the childs
+          // if the childs are leafs
+          return new Integer(childCount);
+        }
+      }
+    };
   }
 
 
@@ -84,188 +86,197 @@ public class Triggers {
    */
   public static VCalculateColumn sumFixed(VReportColumn c) {
     return new VCCDepthFirstCircuitN() {
-	public Object evalNode(VReportRow row, int column) {
-	  int			childCount = row.getChildCount();
-	  com.kopiright.xkopi.lib.type.NotNullFixed	result = new com.kopiright.xkopi.lib.type.NotNullFixed(0, 2);
+      public Object evalNode(VReportRow row, int column) {
+        int             childCount = row.getChildCount();
+        NotNullFixed    result = new NotNullFixed(0, 2);
 
-	  for (int i = 0; i < childCount; i++) {
-	    VReportRow	child = (VReportRow)row.getChildAt(i);
-	    com.kopiright.xkopi.lib.type.NotNullFixed value = (com.kopiright.xkopi.lib.type.NotNullFixed)child.getValueAt(column);
-	    if  (value != null) {
-	      result = result.add(value);
-	    }
-	  }
-	  return result;
-	}
-      };
+        for (int i = 0; i < childCount; i++) {
+          VReportRow    child = (VReportRow)row.getChildAt(i);
+          NotNullFixed value = (NotNullFixed)child.getValueAt(column);
+
+          if  (value != null) {
+            result = result.add(value);
+          }
+        }
+        return result;
+      }
+    };
   }
+
 
   /**
    * Compute the integer sum in a report column
    */
   public static VCalculateColumn sumNullInteger(VReportColumn c) {
     return new VCCDepthFirstCircuitN() {
-	public Object evalNode(VReportRow row, int column) {
-	  int		childCount = row.getChildCount();
-	  int		result = 0;
-	  boolean valueFound = false;
+      public Object evalNode(VReportRow row, int column) {
+        int     childCount = row.getChildCount();
+        int     result = 0;
+        boolean valueFound = false;
 
-	  for (int i = 0; i < childCount; i++) {
-	    VReportRow	child = (VReportRow)row.getChildAt(i);
-	    Integer	value = (Integer)child.getValueAt(column);
-	    if  (value != null) {
-              valueFound = true;
-	      result += value.intValue();
-	    }
-	  }
-	  return valueFound ? new Integer(result) : null;
-	}
-      };
+        for (int i = 0; i < childCount; i++) {
+          VReportRow    child = (VReportRow)row.getChildAt(i);
+          Integer       value = (Integer)child.getValueAt(column);
+
+          if (value != null) {
+            valueFound = true;
+            result += value.intValue();
+          }
+        }
+        return valueFound ? new Integer(result) : null;
+      }
+    };
   }
+
 
   /**
    * Compute the fixed sum in a report column
    */
   public static VCalculateColumn sumNullFixed(VReportColumn c) {
     return new VCCDepthFirstCircuitN() {
-	public Object evalNode(VReportRow row, int column) {
-	  int childCount = row.getChildCount();
-	  boolean valueFound = false;
-	  com.kopiright.xkopi.lib.type.NotNullFixed	result = new com.kopiright.xkopi.lib.type.NotNullFixed(0, 2);
+      public Object evalNode(VReportRow row, int column) {
+        int childCount = row.getChildCount();
+        boolean         valueFound = false;
+        NotNullFixed    result = new NotNullFixed(0, 2);
 
-	  for (int i = 0; i < childCount; i++) {
-	    VReportRow	child = (VReportRow)row.getChildAt(i);
-	    com.kopiright.xkopi.lib.type.NotNullFixed value = (com.kopiright.xkopi.lib.type.NotNullFixed)child.getValueAt(column);
-	    if  (value != null) {
-              valueFound = true;
-	      result = result.add(value);
-	    }
-	  }
+        for (int i = 0; i < childCount; i++) {
+          VReportRow    child = (VReportRow)row.getChildAt(i);
+          NotNullFixed value = (NotNullFixed)child.getValueAt(column);
 
-	  return valueFound ? result : null;
-	}
-      };
+          if  (value != null) {
+            valueFound = true;
+            result = result.add(value);
+          }
+        }
+        return valueFound ? result : null;
+      }
+    };
   }
+
 
   /**
    * Report a value when all child are identical
    */
   public static VCalculateColumn reportIdenticalValue(VReportColumn c) {
     return new VCCDepthFirstCircuitN() {
-	public Object evalNode(VReportRow row, int column) {
-	  int			childCount = row.getChildCount();
-	  Object		value = ((VReportRow)row.getChildAt(0)).getValueAt(column);
+      public Object evalNode(VReportRow row, int column) {
+        int     childCount = row.getChildCount();
+        Object  value = ((VReportRow)row.getChildAt(0)).getValueAt(column);
 
-	  if (value == null) {
-	    return null;
-	  } else {
-            for (int i = 1; i < childCount; i++) {
-              VReportRow	child = (VReportRow)row.getChildAt(i);
+        if (value == null) {
+          return null;
+        } else {
+          for (int i = 1; i < childCount; i++) {
+            VReportRow  child = (VReportRow)row.getChildAt(i);
 
-              if (!value.equals(child.getValueAt(column))) {
-                return null;
-              }
+            if (!value.equals(child.getValueAt(column))) {
+              return null;
             }
-            return value;
           }
-	}
-      };
+          return value;
+        }
+      }
+    };
   }
+
 
   /**
    * Compute the integer average in a report column
    */
   public static VCalculateColumn avgInteger(VReportColumn c) {
     return new VCCDepthFirstCircuitN() {
-	public Object evalNode(VReportRow row, int column) {
-	  int		childCount = row.getChildCount();
-	  int		result = 0;
+      public Object evalNode(VReportRow row, int column) {
+        int     childCount = row.getChildCount();
+        int     result = 0;
 
-	  for (int i = 0; i < childCount; i++) {
-	    VReportRow	child = (VReportRow)row.getChildAt(i);
-	    Integer value = (Integer)child.getValueAt(column);
-	    if  (value != null) {
-	      result += value.intValue();
-	    }
-	  }
+        for (int i = 0; i < childCount; i++) {
+          VReportRow    child = (VReportRow)row.getChildAt(i);
+          Integer value = (Integer)child.getValueAt(column);
 
-	  return new Integer(result / childCount);
-	}
-      };
+          if  (value != null) {
+            result += value.intValue();
+          }
+        }
+        return new Integer(result / childCount);
+      }
+    };
   }
+
+
   /**
    * Compute the integer sum in a report column and the the value
    * in the leaves with a serial number
    */
   public static VCalculateColumn serialInteger(VReportColumn c) {
     return new VCCDepthFirstCircuitN() {
-	public Object evalNode(VReportRow row, int column) {
-	  int		childCount = row.getChildCount();
 
-          if (row.getLevel() > 1) {
-            // the value of a node is the sum of
-            // other nodes (contain no leafs)
-            int         result = 0;
+      public Object evalNode(VReportRow row, int column) {
+        int     childCount = row.getChildCount();
 
-            for (int i = 0; i < childCount; i++) {
-              VReportRow        child = (VReportRow)row.getChildAt(i);
-              Integer           value = (Integer)child.getValueAt(column);
+        if (row.getLevel() > 1) {
+          // the value of a node is the sum of
+          // other nodes (contain no leafs)
+          int   result = 0;
 
-              if  (value != null) {
-                result += value.intValue();
-              }
-            }
-            return new Integer(result);
-          } else {
-            // the value is the number of the childs
-            // if the childs are leafs
-            return new Integer(childCount);
-          }
-	}
+          for (int i = 0; i < childCount; i++) {
+            VReportRow  child = (VReportRow)row.getChildAt(i);
+            Integer     value = (Integer)child.getValueAt(column);
 
-        /**
-         * Add calculated data into the report row
-         */
-        public void calculate(VGroupRow tree, int column) {
-          if (tree.getLevel() > 1) {
-            int	childCount = tree.getChildCount();
-            
-            for (int i = 0; i < childCount; i++) {
-              calculate((VGroupRow)tree.getChildAt(i), column);
-            }
-          } else {
-            int	childCount = tree.getChildCount();
-            
-            for (int i = 0; i < childCount; i++) {
-              // set leave to serial number
-              ((VBaseRow)tree.getChildAt(i)).setValueAt(column, new Integer(i+1));
+            if  (value != null) {
+              result += value.intValue();
             }
           }
-
-          tree.setValueAt(column, evalNode(tree, column));
+          return new Integer(result);
+        } else {
+          // the value is the number of the childs
+          // if the childs are leafs
+          return new Integer(childCount);
         }
-      };
+      }
+
+      /**
+       * Add calculated data into the report row
+       */
+      public void calculate(VGroupRow tree, int column) {
+        if (tree.getLevel() > 1) {
+          int childCount = tree.getChildCount();
+
+          for (int i = 0; i < childCount; i++) {
+            calculate((VGroupRow)tree.getChildAt(i), column);
+          }
+        } else {
+          int childCount = tree.getChildCount();
+
+          for (int i = 0; i < childCount; i++) {
+            // set leave to serial number
+            ((VBaseRow)tree.getChildAt(i)).setValueAt(column, new Integer(i+1));
+          }
+        }
+        tree.setValueAt(column, evalNode(tree, column));
+      }
+    };
   }
+
 
   /**
    * Compute the fixed average in a report column
    */
   public static VCalculateColumn avgFixed(VReportColumn c) {
     return new VCCDepthFirstCircuitN() {
-	public Object evalNode(VReportRow row, int column) {
-	  int			childCount = row.getChildCount();
-	  com.kopiright.xkopi.lib.type.NotNullFixed	result = new com.kopiright.xkopi.lib.type.NotNullFixed(0, 2);
+      public Object evalNode(VReportRow row, int column) {
+        int             leafCount = row.getLeafCount();
+        NotNullFixed    result = new NotNullFixed(0, 2);
+        VReportRow      leaf = (VReportRow)row.getFirstLeaf();
 
-	  for (int i = 0; i < childCount; i++) {
-	    VReportRow	child = (VReportRow)row.getChildAt(i);
-	    com.kopiright.xkopi.lib.type.NotNullFixed value = (com.kopiright.xkopi.lib.type.NotNullFixed)child.getValueAt(column);
-	    if  (value != null) {
-	      result = result.add(value);
-	    }
-	  }
-
-	  return result.divide(new com.kopiright.xkopi.lib.type.NotNullFixed(childCount));
-	}
-      };
+        for (int i = 0; i < leafCount; i++) {
+          NotNullFixed value = (NotNullFixed)leaf.getValueAt(column);
+          if (value != null) {
+            result = result.add(value);
+            leaf = (VReportRow)leaf.getNextLeaf();
+          }
+        }
+        return result.divide(new NotNullFixed(leafCount)).setScale(2);
+      }
+    };
   }
 }
