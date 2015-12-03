6759afd2844ee7c0723dbbe8eb7b484cfba425df
hacheni <hacheni@bf1b744e-a3f0-0310-8f4b-f8bdd3f196b6>
2015-12-03 06:06:29 +0000

    Ext: Handles file filters when selecting a file from the client side by injecting suitable mime types to the File Input HTML component.

--- src/com/kopiright/vkopi/lib/ui/vaadin/visual/VFileHandler.java
+++ src/com/kopiright/vkopi/lib/ui/vaadin/visual/VFileHandler.java
@@ -22,6 +22,10 @@ package com.kopiright.vkopi.lib.ui.vaadin.visual;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.kopiright.vkopi.lib.ui.vaadin.base.FileUploader;
 import com.kopiright.vkopi.lib.visual.ApplicationContext;
@@ -59,17 +63,31 @@ public class VFileHandler extends FileHandler {
   }
 
   @Override
-  public File openFile(UWindow window, final FileFilter filter) {
-    return openFile(window, (String)null); // FIXME: Use the file filter by adding a mime type in the FileFilter object.
+  public File openFile(UWindow window, FileFilter filter) {
+    return openFile(window, null, null, getMimeType(filter));
   }
 
   @Override
   public File openFile(UWindow window, File dir, String defaultName) {
-    byte[]	        file;
+    return openFile(window, dir, defaultName, null);
+  }
+  
+  /**
+   * Uploads a file from client side with the given mime type.
+   * @param window The window display.
+   * @param dir The directory to be used to store the uploaded file.
+   * @param defaultName The file name to be used.
+   * @param mimeType The searched mime type.
+   * @return The uploaded file.
+   */
+  protected File openFile(UWindow window, File dir, String defaultName, String mimeType) {
+    byte[]              file;
+    FileUploader        uploader;
     
-    file = new FileUploader().upload(null); // no mime type is provided 
+    uploader = new FileUploader();
+    file = uploader.upload(mimeType);
     if (file != null) {
-      return toFile(window, file, dir, defaultName);
+      return toFile(window, file, dir, uploader.getFilename() == null ? defaultName : uploader.getFilename());
     }
     
     return null;
@@ -120,10 +138,10 @@ public class VFileHandler extends FileHandler {
     String		basename;
     String		extension;
     
-    basename = getBaseFileName(defaultName);
+    basename = ensurePrefixLength(getBaseFileName(defaultName));
     extension = getExtension(defaultName);
     
-    return File.createTempFile(basename, String.valueOf("." + extension), directory);
+    return File.createTempFile(basename, extension, directory);
   }
   
   /**
@@ -136,11 +154,11 @@ public class VFileHandler extends FileHandler {
       int	index = defaultName.lastIndexOf('.');
       
       if (index != -1) {
-	return defaultName.substring(Math.min(defaultName.length(), index + 1));
+	return "." + defaultName.substring(Math.min(defaultName.length(), index + 1));
       }
     }
     
-    return ""; // no extension.
+    return null; // ".tmp" will be added when calling File#createTempFile(String, String, File).
   }
   
   /**
@@ -157,6 +175,154 @@ public class VFileHandler extends FileHandler {
       }
     }
     
-    return ""; // empty name.
+    return defaultName; // returns the entire default name.
+  }
+  
+  /**
+   * When calling {@link File#createTempFile(String, String, File)} the name
+   * prefix should at least have 3 characters. This method aims to satisfy this
+   * condition and complete missing length with X character to avoid {@link IllegalArgumentException}
+   * when creating temporary files. 
+   * @param prefix The file name prefix.
+   * @return A prefix having at least three characters. If {@code prefix == null}, "XXX" is returned.
+   */
+  protected String ensurePrefixLength(String prefix) {
+    if (prefix != null && prefix.length() > 3) {
+      return prefix;
+    } else {
+      StringBuffer      buffer;
+      
+      buffer = new StringBuffer();
+      buffer.append(prefix);
+      while (buffer.length() < 3) {
+        buffer.insert(prefix.length(), "X");
+      }
+      
+      return buffer.toString();
+    }
+  }
+  
+  /**
+   * Returns the mime type provided by the given file filter.
+   * The technique is to loop all over known mime types and to
+   * test the if the file is accepted by the filter. The corresponding
+   * mime type is returned.
+   * If filter provide unknown file, {@code null} is returned.  
+   * @param filter The file filter.
+   * @return The corresponding mime type.
+   */
+  private static String getMimeType(FileFilter filter) {
+    List<String>                mimeTypes;
+    
+    mimeTypes = new ArrayList<String>();
+    for (Map.Entry<String, String> entry : knownFileTypeToMimeType.entrySet()) {
+      /**
+       * A dummy file is created. It does not matter if file exists or not.
+       * Generally, file filter tests on file extensions.
+       */
+      if (filter.accept(new File("file" + "." + entry.getKey()))) {
+        // we return the first mime type that fits with the filter
+        mimeTypes.add(entry.getValue());
+      }
+    }
+    
+    return toString(mimeTypes);
+  }
+  
+  /**
+   * Converts the given list to a string where elements are separated by a comma.
+   * @param list The list of strings.
+   * @return The separated comma string.
+   */
+  private static String toString(List<String> list) {
+    if (list == null || list.isEmpty()) {
+      return null;
+    } else {
+      StringBuffer        buffer;
+
+      buffer = new StringBuffer();
+      for (int i = 0; i < list.size(); i++) {
+        buffer.append(list.get(i));
+        if (i != list.size() -1) {
+          buffer.append(", ");
+        }
+      }
+      
+      return buffer.toString();
+    }
+  }
+  
+  // --------------------------------------------------
+  // DATA MEMBERS
+  // --------------------------------------------------
+  
+  /**
+   * File type to mime type map
+   * This will be used to inject mime type according to a given
+   * file filter.
+   * This map will be initialized with known mime types. If a file filter
+   * comes with unknown type, {@code null} will be returned as mime type.
+   */
+  private static Map<String, String>            knownFileTypeToMimeType;
+  
+  static {
+    /**
+     * The injected of list of mime types are picked form 
+     * http://www.sitepoint.com/web-foundations/mime-types-summary-list/
+     */
+    knownFileTypeToMimeType = new HashMap<String, String>();
+    knownFileTypeToMimeType.put("au", "audio/basic");
+    knownFileTypeToMimeType.put("avi", "video/msvideo, video/avi, video/x-msvideo, .avi");
+    knownFileTypeToMimeType.put("bmp", "image/bmp");
+    knownFileTypeToMimeType.put("bz2", "application/x-bzip2");
+    knownFileTypeToMimeType.put("css", "text/css");
+    knownFileTypeToMimeType.put("dtd", "application/xml-dtd");
+    knownFileTypeToMimeType.put("doc", "application/msword, .doc");
+    knownFileTypeToMimeType.put("docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document, .docx");
+    knownFileTypeToMimeType.put("dotx", "application/vnd.openxmlformats-officedocument.wordprocessingml.template, .dotx");
+    knownFileTypeToMimeType.put("es", "application/ecmascript");
+    knownFileTypeToMimeType.put("exe", "application/octet-stream");
+    knownFileTypeToMimeType.put("gif", "image/gif");
+    knownFileTypeToMimeType.put("gz", "application/x-gzip");
+    knownFileTypeToMimeType.put("hqx", "application/mac-binhex40");
+    knownFileTypeToMimeType.put("html", "text/html");
+    knownFileTypeToMimeType.put("jar", "application/java-archive");
+    knownFileTypeToMimeType.put("jpg", "image/jpeg");
+    knownFileTypeToMimeType.put("js", "application/x-javascript");
+    knownFileTypeToMimeType.put("midi", "audio/x-midi");
+    knownFileTypeToMimeType.put("mp3", "audio/mpeg");
+    knownFileTypeToMimeType.put("mpeg", "video/mpeg");
+    knownFileTypeToMimeType.put("ogg", "audio/vorbis, application/ogg");
+    knownFileTypeToMimeType.put("pdf", "application/pdf");
+    knownFileTypeToMimeType.put("pl", "application/x-perl");
+    knownFileTypeToMimeType.put("png", "image/png");
+    knownFileTypeToMimeType.put("potx", "application/vnd.openxmlformats-officedocument.presentationml.template");
+    knownFileTypeToMimeType.put("ppsx", "application/vnd.openxmlformats-officedocument.presentationml.slideshow");
+    knownFileTypeToMimeType.put("ppt", "application/vnd.ms-powerpointtd, .ppt");
+    knownFileTypeToMimeType.put("pptx", "application/vnd.openxmlformats-officedocument.presentationml.presentation, .pptx");
+    knownFileTypeToMimeType.put("ps", "application/postscript");
+    knownFileTypeToMimeType.put("qt", "video/quicktime");
+    knownFileTypeToMimeType.put("ra", "audio/x-pn-realaudio, audio/vnd.rn-realaudio");
+    knownFileTypeToMimeType.put("ram", "audio/x-pn-realaudio, audio/vnd.rn-realaudio");
+    knownFileTypeToMimeType.put("rdf", "application/rdf, application/rdf+xml");
+    knownFileTypeToMimeType.put("rtf", "application/rtf");
+    knownFileTypeToMimeType.put("sgml", "text/sgml");
+    knownFileTypeToMimeType.put("sit", "application/x-stuffit");
+    knownFileTypeToMimeType.put("sldx", "application/vnd.openxmlformats-officedocument.presentationml.slide");
+    knownFileTypeToMimeType.put("svg", "image/svg+xml");
+    knownFileTypeToMimeType.put("swf", "application/x-shockwave-flash");
+    knownFileTypeToMimeType.put("tar.gz", "application/x-tar");
+    knownFileTypeToMimeType.put("tgz", "application/x-tar");
+    knownFileTypeToMimeType.put("tiff", "image/tiff");
+    knownFileTypeToMimeType.put("tsv", "text/tab-separated-values");
+    knownFileTypeToMimeType.put("txt", "text/plain");
+    knownFileTypeToMimeType.put("wav", "audio/wav, audio/x-wav");
+    knownFileTypeToMimeType.put("xlam", "application/vnd.ms-excel.addin.macroEnabled.12");
+    knownFileTypeToMimeType.put("xls", "application/vnd.ms-excel, .xls");
+    knownFileTypeToMimeType.put("xlsb", "application/vnd.ms-excel.sheet.binary.macroEnabled.12");
+    knownFileTypeToMimeType.put("xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, .xlsx");
+    knownFileTypeToMimeType.put("xltx", "application/vnd.openxmlformats-officedocument.spreadsheetml.template");
+    knownFileTypeToMimeType.put("xml", "application/xml");
+    knownFileTypeToMimeType.put("zip", "application/zip, application/x-compressed-zip");
   }
 }
